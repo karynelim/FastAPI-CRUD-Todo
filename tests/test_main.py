913831a7fb7bac92fae54d0095a2ead73af4b7e3
Karyne <karynelima20@gmail.com>
2025-08-17 17:04:52 -0300

    Add gitgnore

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,7 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the Enhanced FastAPI Todo App!"}
+
+    #pytest -s tests/test_main.py                                                                                                                            [ 16%]
+    #pytest -s tests/test_todo.py
+    
